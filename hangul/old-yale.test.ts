e2030b8d0b6090e19b6292402d5639ce4634cb61
kiwiyou <kiwiyou@kiwiyou.dev>
2025-03-12 03:30:18 +0900

    fix: set default normalization mode

--- hangul/old-yale.test.ts
+++ hangul/old-yale.test.ts
@@ -2,45 +2,45 @@ import { assertEquals } from "jsr:@std/assert";
 import { oldYaleToHangul } from "./old-yale.ts";
 
 Deno.test("normalizable choseong", () => {
-  assertEquals(oldYaleToHangul("ch"), "\u314A");
+  assertEquals(oldYaleToHangul("ch", true), "\u314A");
 });
 
 Deno.test("normalizable choseong-jungseong", () => {
-  assertEquals(oldYaleToHangul("hay"), "\uD574");
+  assertEquals(oldYaleToHangul("hay", true), "\uD574");
 });
 
 Deno.test("normalizable choseong-jungseong-jongseong", () => {
-  assertEquals(oldYaleToHangul("phwoG"), "\uD401");
+  assertEquals(oldYaleToHangul("phwoG", true), "\uD401");
 });
 
 Deno.test("not normalizable choseong", () => {
-  assertEquals(oldYaleToHangul("sk"), "\u112D");
+  assertEquals(oldYaleToHangul("sk", true), "\u112D");
 });
 
 Deno.test("not normalizable choseong-jungseong", () => {
-  assertEquals(oldYaleToHangul("ss\\yoyay"), "\u113F\u1185");
+  assertEquals(oldYaleToHangul("ss\\yoyay", true), "\u113F\u1185");
 });
 
 Deno.test("not normalizable choseong-jungseong-jongseong", () => {
-  assertEquals(oldYaleToHangul("PHuywungz"), "\u1157\u1197\u11F2");
+  assertEquals(oldYaleToHangul("PHuywungz", true), "\u1157\u1197\u11F2");
 });
 
 Deno.test("normalizable filler-jungseong", () => {
-  assertEquals(oldYaleToHangul("`wo"), "\u3157");
+  assertEquals(oldYaleToHangul("`wo", true), "\u3157");
 });
 
 Deno.test("skip standalone filler", () => {
-  assertEquals(oldYaleToHangul("`"), "`");
+  assertEquals(oldYaleToHangul("`", true), "`");
 });
 
 Deno.test("front separator", () => {
-  assertEquals(oldYaleToHangul(".ch"), "\u314A");
+  assertEquals(oldYaleToHangul(".ch", true), "\u314A");
 });
 
 Deno.test("middle separator", () => {
-  assertEquals(oldYaleToHangul("ch.ch"), "\u314A\u314A");
+  assertEquals(oldYaleToHangul("ch.ch", true), "\u314A\u314A");
 });
 
 Deno.test("back separator", () => {
-  assertEquals(oldYaleToHangul("ch."), "\u314A.");
+  assertEquals(oldYaleToHangul("ch.", true), "\u314A.");
 });
